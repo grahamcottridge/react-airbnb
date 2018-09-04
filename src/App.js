af2829a4ec412dd95458b36497c5e3aea5aa5933
Graham Cottridge <codinggc@gmail.com>
2018-09-04 22:41:32 +0100

    Render flats from json

--- src/App.js
+++ src/App.js
@@ -5,15 +5,41 @@ import './App.css';
 import Flat from './components/flat'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        })
+      })
+  }
+
   render() {
-
     return (
-      <div>
-      <Flat flat={flat} />
-
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+          <div className="map">
+          </div>
       </div>
     );
   }
 }
 
 export default App;
+
